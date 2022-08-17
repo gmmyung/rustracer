450b7c6b522c56f1b628d3225aa7f9e06f1b7bed
Gyungmin Myung <ryan000502@gmail.com>
2022-08-17 12:09:40 +0900

    해치웠.....나?

--- src/object.rs
+++ src/object.rs
@@ -1,7 +1,25 @@
 use crate::math::{Ray, Vec3, Float};
 
 pub trait Hittable {
-    fn hit(&self, h: &HitAttr) -> Option<HitAttr>;
+    fn hit(&self, h: &HitAttr) -> Option<HitAttr>{
+        if let Some(t) = self.intersect(h) {
+            Some(HitAttr{
+                t, 
+                ray: Ray { 
+                    origin: h.ray.at(t), 
+                    direction: self.normal(h).random_diffusion(), 
+                    color: self.get_color().mul(&h.ray.color)
+                },
+                prev_hit_index: h.prev_hit_index,
+            })
+        } else {
+            None
+        }
+    }
+
+    fn normal(&self, h: &HitAttr) -> Vec3;
+    fn intersect(&self, h: &HitAttr) -> Option<Float>;
+    fn get_color(&self) -> Vec3;
 }
 
 #[derive(Clone, Copy)]
@@ -24,31 +42,30 @@ impl Sphere{
 }
 
 impl Hittable for Sphere {
-    fn hit(&self, h: &HitAttr) -> Option<HitAttr> {
+    fn intersect(&self, h: &HitAttr) -> Option<Float> {
         let oc = h.ray.origin - self.center;
         let a = h.ray.direction.dot(&h.ray.direction);
         let b = oc.dot(&h.ray.direction);
         let c = oc.dot(&oc) - self.radius * self.radius;
         let discriminant = b * b - a * c;
-        let normal = (h.ray.origin - self.center) * (1.0 / self.radius);
         if discriminant > 0.0 {
-            let t = (- b - discriminant.sqrt()) / a;
-            if t < 0.0 {
-                None
+            let temp = (-b - discriminant.sqrt()) / a;
+            if temp > 0.0 {
+                Some(temp)
             } else {
-                Some(HitAttr{
-                    t, 
-                    ray: Ray { 
-                        origin: h.ray.at(t), 
-                        direction: normal.random_diffusion(), 
-                        color: self.color.mul(&h.ray.color)
-                    },
-                    prev_hit_index: h.prev_hit_index,
-                })
+                None
             }
         } else {
             None
         }
+    }
+
+    fn normal(&self, h: &HitAttr) -> Vec3 {
+        (h.ray.origin - self.center) * (1.0 / self.radius)
+    }
+
+    fn get_color(&self) -> Vec3 {
+        self.color
     }
 }
 
@@ -65,25 +82,25 @@ impl Floor {
 }
 
 impl Hittable for Floor {
-    fn hit(&self, h: &HitAttr) -> Option<HitAttr> {
+
+    fn intersect(&self, h: &HitAttr) -> Option<Float> {
         let t = (self.height - h.ray.origin.z) / h.ray.direction.z;
-        let normal = if self.upwards {
+        if t > 0.0 {
+            Some(t)
+        } else {
+            None
+        }
+    }
+
+    fn normal(&self, _h: &HitAttr) -> Vec3 {
+        if self.upwards {
             Vec3::new(0.0, 0.0, 1.0)
         } else {
             Vec3::new(0.0, 0.0, -1.0)
-        };
-        if t < 0.0 {
-            None
-        } else {
-            Some(HitAttr{
-                t, 
-                ray: Ray { 
-                    origin: h.ray.at(t), 
-                    direction: normal.random_diffusion(), 
-                    color: h.ray.color.mul(&self.color)
-                },
-                prev_hit_index: h.prev_hit_index
-            })
         }
+    }
+
+    fn get_color(&self) -> Vec3 {
+        self.color
     }
 }
